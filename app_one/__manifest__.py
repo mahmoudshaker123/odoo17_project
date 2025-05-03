4332053ead2dad488b2646fe0d07cd300e59077c
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-05-04 00:12:04 +0300

    inhertence

--- app_one/__manifest__.py
+++ app_one/__manifest__.py
@@ -5,13 +5,15 @@
     'author': "Mahmoud Shaker",
     'category': '',
     'version': '17.0.0.1.0',
-    'depends': ['base'],
+    'depends': ['base' , 'sale_management' , 'account' ,'sale'
+                ],
     'data': [
         'security/ir.model.access.csv',
         'views/base_menu.xml',
         'views/property_view.xml',
         'views/owner_view.xml',
-        'views/tag_view.xml'
+        'views/tag_view.xml',
+        'views/sale_order_view.xml',
 
 
 ],
---
+++ app_one/models/sale_order.py
@@ -0,0 +1,13 @@
+from odoo import models , fields
+
+
+class SaleOder(models.Model):
+    _inherit = 'sale.order'
+
+    property_id= fields.Many2one('property')
+
+    def action_confirm(self):
+        res = super(SaleOder, self).action_confirm()
+        print("inside action_confirm method ")
+        return res
+
