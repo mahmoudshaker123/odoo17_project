840f509420c3ba6b1e28ffdc64372e301df7c79a
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-06-07 21:44:28 +0300

    Create Task Two IET

--- app_one/models/property.py
+++ app_one/models/property.py
@@ -146,6 +146,13 @@ class Property(models.Model):
         action['context']={'default_property_id':self.id}
         return action
 
+    def action_open_related_owner(self):
+        action = self.env['ir.actions.actions']._for_xml_id('app_one.owner_action')
+        view_id = self.env.ref('app_one.owner_view_form').id
+        action['res_id']=self.owner_id.id
+        action['views']=[[view_id , 'form']]
+        return  action
+
 
 
 
