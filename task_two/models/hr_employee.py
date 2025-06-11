c1cf06ec0321b2234592a3927ea802f84a1fc644
mahmoudshaker123 <mahmoud.shaker123123@gmail.com>
2025-06-11 20:55:32 +0300

    Add Smart Button Task Two IET

--- task_two/models/hr_employee.py
+++ task_two/models/hr_employee.py
@@ -10,6 +10,28 @@ class HrEmployee(models.Model):
         ('unique_employee_number', 'unique("employee_number")', 'This employee number is exist')
     ]
 
+    def action_open_related_task(self):
+        action = self.env['ir.actions.actions']._for_xml_id('task_two.task_management_action')
+        tree_view_id = self.env.ref('task_two.task_management_tree_view').id
+        form_view_id = self.env.ref('task_two.task_management_form_view').id
+
+        action.update({
+            'domain': [
+                ('employee', '=', self.id),
+                ('state', '=', 'in_progress')
+            ],
+            'context': {
+                'default_employee': self.id
+            },
+            'views': [
+                (tree_view_id, 'tree'),
+                (form_view_id, 'form')
+            ],
+            'view_mode': 'tree,form',
+        })
+        return action
+
+
 
 
 
